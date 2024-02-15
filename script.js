87db1fcb96f742dd55f0168fd2254ec90dc33dd9
Andrew Todd <andrewdanieltodd@gmail.com>
2024-02-15 02:17:30 -0700

    Added the files for the lab

---
+++ script.js
@@ -0,0 +1,8 @@
+function drawTriangle(triangleSize) {
+
+   // Your solution goes here
+   for(let i = 1; i <= triangleSize; i++) {
+       let asterisks = '*'.repeat(i);
+       console.log(asterisks);
+    }
+}
